8817aa6cc026b54018087d6c8f972c4bc4d31dc2
Jonas Adler <jonasadl@kth.se>
2016-11-25 11:47:47 +0100

    WIP: Add affine group mapping

--- lie_group_diffeo/matrix_group.py
+++ lie_group_diffeo/matrix_group.py
@@ -5,7 +5,9 @@ from lie_group import LieGroup, LieGroupElement, LieAlgebra, LieAlgebraElement
 from action import LieAction
 
 
-__all__ = ('GLn', 'SOn', 'MatrixVectorAction', 'MatrixImageAction')
+__all__ = ('GLn', 'SOn',
+           'MatrixVectorAction', 'MatrixImageAction',
+           'MatrixVectorAffineAction')
 
 
 class MatrixGroup(LieGroup):
@@ -148,6 +150,42 @@ class SOnAlgebraElement(MatrixAlgebraElement):
     pass
 
 
+class AffineGroup(MatrixGroup):
+    """Affine group represented via matrices.
+
+    The matrices have the form::
+
+        A v
+        0 1
+    """
+    @property
+    def associated_algebra(self):
+        return AffineGroupAlgebra(self)
+
+    @property
+    def element_type(self):
+        return AffineGroupElement
+
+
+class AffineGroupElement(MatrixGroupElement):
+    pass
+
+
+class AffineGroupAlgebra(MatrixAlgebra):
+    @property
+    def element_type(self):
+        return AffineAlgebraElement
+
+    def project(self, array):
+        cpy = array.copy()
+        cpy[-1] = 0
+        return cpy
+
+
+class AffineAlgebraElement(MatrixAlgebraElement):
+    pass
+
+
 class MatrixVectorAction(LieAction):
     """Action by matrix vector product."""
 
@@ -156,14 +194,18 @@ class MatrixVectorAction(LieAction):
         assert lie_group.size == domain.size
 
     def action(self, lie_grp_element):
+        assert lie_grp_element in self.lie_group
         return odl.MatVecOperator(lie_grp_element.arr,
                                   self.domain, self.domain)
 
-    def inf_action(self, lie_grp_element):
-        return odl.MatVecOperator(lie_grp_element.arr,
+    def inf_action(self, lie_alg_element):
+        assert lie_alg_element in self.lie_group.associated_algebra
+        return odl.MatVecOperator(lie_alg_element.arr,
                                   self.domain, self.domain)
 
     def inf_action_adj(self, v, m):
+        assert v in self.domain
+        assert m in self.domain
         return self.lie_group.associated_algebra.element(np.outer(m, v))
 
 
@@ -179,19 +221,23 @@ class MatrixImageAction(LieAction):
             self.gradient = gradient
 
     def action(self, lie_grp_element):
+        assert lie_grp_element in self.lie_group
         pts = self.domain.points()
         deformed_pts = lie_grp_element.arr.dot(pts.T) - pts.T
         deformed_pts = self.domain.tangent_bundle.element(deformed_pts)
         return odl.deform.LinDeformFixedDisp(deformed_pts)
 
-    def inf_action(self, lie_grp_element):
+    def inf_action(self, lie_alg_element):
+        assert lie_alg_element in self.lie_group.associated_algebra
         pts = self.domain.points()
-        deformed_pts = lie_grp_element.arr.dot(pts.T) - pts.T
+        deformed_pts = lie_alg_element.arr.dot(pts.T) - pts.T
         deformed_pts = self.domain.tangent_bundle.element(deformed_pts)
         pointwise_inner = odl.PointwiseInner(self.gradient.range, deformed_pts)
         return pointwise_inner * self.gradient
 
     def inf_action_adj(self, v, m):
+        assert v in self.domain
+        assert m in self.domain
         size = self.lie_group.size
         pts = self.domain.tangent_bundle.element(self.domain.points().T)
         gradv = self.gradient(v)
@@ -199,5 +245,82 @@ class MatrixImageAction(LieAction):
         for i in range(size):
             for j in range(size):
                 result[i, j] = m.inner(gradv[i] * pts[j])
+
+        return self.lie_group.associated_algebra.element(result)
+
+
+class MatrixVectorAffineAction(LieAction):
+    """Action by affine matrix vector product."""
+
+    def __init__(self, lie_group, domain):
+        LieAction.__init__(self, lie_group, domain)
+        assert lie_group.size == domain.size + 1
+
+    def action(self, lie_grp_element):
+        assert lie_grp_element in self.lie_group
+        matrix_op = odl.MatVecOperator(lie_grp_element.arr[:-1, :-1],
+                                       self.domain, self.domain)
+        translation_vec = self.domain.element(lie_grp_element.myarray[:-1, -1])
+        return matrix_op + translation_vec
+
+    def inf_action(self, lie_alg_element):
+        assert lie_alg_element in self.lie_group.associated_algebra
+        matrix_op = odl.MatVecOperator(lie_alg_element.arr[:-1, :-1],
+                                       self.domain, self.domain)
+        translation_vec = self.domain.element(lie_alg_element.myarray[:-1, -1])
+        return matrix_op + translation_vec
+
+    def inf_action_adj(self, v, m):
+        assert v in self.domain
+        assert m in self.domain
+        v_appended = np.ones(v.size + 1)
+        v_appended[:-1] = v
+        m_appended = np.ones(m.size + 1)
+        m_appended[:-1] = m
+        outer = np.outer(m_appended, v_appended)
+        return self.lie_group.associated_algebra.element(outer)
+
+
+class MatrixImageAffineAction(LieAction):
+    """Action on image via affine coordinate transformation."""
+
+    def __init__(self, lie_group, domain, gradient=None):
+        LieAction.__init__(self, lie_group, domain)
+        assert lie_group.size == domain.ndim + 1
+        if gradient is None:
+            self.gradient = odl.Gradient(self.domain)
+        else:
+            self.gradient = gradient
+
+    def action(self, lie_grp_element):
+        assert lie_grp_element in self.lie_group
+        pts = self.domain.points()
+        deformed_pts = lie_grp_element.arr[:-1, :-1].dot(pts.T) - pts.T
+        deformed_pts += lie_grp_element.arr[:-1, -1]
+        deformed_pts = self.domain.tangent_bundle.element(deformed_pts)
+        return odl.deform.LinDeformFixedDisp(deformed_pts)
+
+    def inf_action(self, lie_alg_element):
+        assert lie_alg_element in self.lie_group.associated_algebra
+        pts = self.domain.points()
+        deformed_pts = lie_alg_element.arr[:-1, :-1].dot(pts.T) - pts.T
+        deformed_pts += lie_alg_element.arr[:-1, -1]
+        deformed_pts = self.domain.tangent_bundle.element(deformed_pts)
+        pointwise_inner = odl.PointwiseInner(self.gradient.range, deformed_pts)
+        return pointwise_inner * self.gradient
+
+    def inf_action_adj(self, v, m):
+        assert v in self.domain
+        assert m in self.domain
+        size = self.lie_group.size
+        pts = self.domain.tangent_bundle.element(self.domain.points().T)
+        gradv = self.gradient(v)
+        result = np.zeros([size, size])
+        for i in range(size - 1):
+            for j in range(size - 1):
+                result[i, j] = m.inner(gradv[i] * pts[j])
+
+        for i in range(size - 1):
+            result[i, -1] = m.inner(gradv[i])
 
         return self.lie_group.associated_algebra.element(result)
