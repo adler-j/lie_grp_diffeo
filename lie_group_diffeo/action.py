ce936e58ef101681761ebb0a55b6e61b734e09c8
Jonas Adler <jonasadl@kth.se>
2016-12-01 17:11:47 +0100

    ENH: Add InverseAction

--- lie_group_diffeo/action.py
+++ lie_group_diffeo/action.py
@@ -10,7 +10,7 @@ from numbers import Integral
 
 
 __all__ = ('LieAction', 'ProductSpaceAction', 'IdentityAction',
-           'ComposedAction')
+           'ComposedAction', 'InverseAction')
 
 
 class LieAction(object):
@@ -136,3 +136,21 @@ class ComposedAction(LieAction):
         momentum_map_inner = self.inner.momentum_map(v, m)
         momentum_map_outer = self.outer.momentum_map(v, m)
         return momentum_map_inner + momentum_map_outer
+
+
+class InverseAction(LieAction):
+
+    """The apply action, but with the inverse element."""
+
+    def __init__(self, base_action):
+        assert isinstance(base_action, LieAction)
+        LieAction.__init__(self, base_action.lie_group, base_action.domain)
+
+    def action(self, lie_grp_element):
+        return self.base_action.action(lie_grp_element.inverse)
+
+    def inf_action(self, lie_alg_element):
+        return -self.base_action.inf_action(lie_alg_element.inverse)
+
+    def momentum_map(self, v, m):
+        return -self.base_action.momentum_map(v, m)
